a12db62ca6eb53e7a98048f2d93310d160f492eb
David McGrann <dpmcgrann@gmail.com>
2018-02-25 20:16:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,10 @@ def oxford_comma(array)
     "#{array[0]}"
   elsif array.length === 2
     array.join(" and ")
+  elsif array.length === 3
+    array1 = "and #{array[-1]}"
+    array.pop
+    array.push(array1)
+    array.join(",")
   end
 end
